585b72e31576f0590a3be8a1281fb3bdfb827c5c
Patrick Collins <pcollins1@gmail.com>
2022-05-18 13:40:35 +1000

    updated circuit image

---
+++ test2.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iostream>
+
+// using namespace std;
+int main() {
+    float f;
+
+    f = .5;
+    std::cout << f <<" "<< 0.3*pow(f,4) << std::endl;
+
+    f = -.5;
+    std::cout << f <<" "<< 0.3*pow(f,4) << std::endl;
+
+    f = 3;
+    std::cout << f << " "<< 0.3*pow(f,4) << std::endl;
+
+}
